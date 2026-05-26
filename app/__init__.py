9bfaf12f649de11c7e004f5c5cf7d4901c339ef8
Yousef Ali <YousefAli.UK@gmail.com>
2026-05-26 17:08:50 +0100

    feat: implement base template, index page, and initial styles for the UniCycle marketplace.

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ csrf = CSRFProtect()
 migrate = Migrate()
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "60 per hour"],
+    default_limits=["1000 per day", "100 per hour"],
     storage_uri="memory://",
 )
 
